58d7785d02639405a13bd36a9842e44e7301f340
Harvinder Atwal <harvinder@harvinderatwal.com>
2026-07-13 22:48:35 +0100

    fix(tf): relax API Gateway Lambda invocation permission to prevent proxy integration failures

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -94,5 +94,4 @@ resource "aws_lambda_permission" "api_gateway_permission" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.api_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
