2a78274451dd28f7f0347d4ac26d5537ea0719da
Harvinder Atwal <harvinder@harvinderatwal.com>
2026-07-15 23:31:08 +0100

    fix(web): import config.js inside admin portal and persist active GA key

--- web/js/config.js
+++ web/js/config.js
@@ -5,7 +5,7 @@
 
 window.EMF_CONFIG = {
   // Google Analytics 4 (GA4) Measurement ID (e.g. "G-XXXXXXXXXX")
-  google_analytics_id: "G-XXXXXXXXXX",
+  google_analytics_id: "G-6SCE8WM3V0",
 
   // W3C Web Push VAPID Public Key (Application Server Key)
   vapid_public_key: "BK2lSzjXWyWtMfPrPJXu-RkBAL1qT5eZCZeWoxAkU_zdq-S_igHvwj-O_qHr8o2_-dqHF62CyYbGinCtwCwMy64"
